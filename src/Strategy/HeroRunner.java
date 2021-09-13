5e5c39b78d35d23e9844125d128ab7d0e4d80b5d
Lera <valmarfina@gmail.com>
2021-09-13 20:39:59 +0300

    added event handler

--- src/Strategy/HeroRunner.java
+++ src/Strategy/HeroRunner.java
@@ -1,7 +1,7 @@
 package Strategy;
 
 public class HeroRunner {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     Hero myHero = new Hero(new Point(0,0), new Flying());
 
     myHero.move(new Point(1,4));
